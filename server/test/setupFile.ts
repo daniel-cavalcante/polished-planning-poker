2b410d701cfeeec109be0a84e8b2d2052a12a971
Daniel Cavalcante <danielcavalcante@protonmail.com>
2023-01-29 10:24:03 -0300

    Remove unnecessary comments

--- server/test/setupFile.ts
+++ server/test/setupFile.ts
@@ -5,13 +5,11 @@ const URI = process.env.MONGO_URI;
 mongoose.set("strictQuery", true);
 
 beforeAll(async () => {
-  // put your client connection code here, example with mongoose:
   if (URI) {
     await mongoose.connect(URI);
   }
 });
 
 afterAll(async () => {
-  // put your client disconnection code here, example with mongodb:
   await mongoose.disconnect();
 });
